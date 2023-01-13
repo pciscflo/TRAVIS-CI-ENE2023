2cedd8cb5cdb7865a4d645aa1fa218ad884526d9
professor <pciscflo@upc.edu.pe>
2023-01-12 21:25:03 -0500

    07.00

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,7 +11,7 @@ var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
+    return "GET - Net 6 Running TRAVIS ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
 });
 
 app.MapPost("/", () =>
